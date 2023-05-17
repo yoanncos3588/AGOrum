654edb8e93296e434f59ca17ca7fc6da92596859
YoannCos <yoanncos@gmail.com>
2023-05-17 19:05:13 +0200

    ajout de prisma et definition du schema

---
+++ doc/user-stories.md
@@ -0,0 +1,36 @@
+# USER STORIES
+
+---
+
+Intro : exemple de découpage
+
+Index
+    - Catégories 0-n
+        - Sujets 0-n
+            - Discussions 0-n
+
+---
+
+## Un utilisateur peut :
+- Accéder à une page d'accueil avec la liste des catégories de discussions
+- Se connecter à un compte utilisateur avec son email + mdp
+- Créer un sujet de discussion
+- Répondre à un sujet de discussion existant
+- Modifier son message avec une mention de la date de modification
+- Réagir à un message via un système d'humeur
+- Modifier ses informations de profils
+- Envoyer un message privé à un autre utilisateur
+- Signaler un message aux administrateurs
+
+## Un modérateur peut :
+- faire toutes les actions possibles par un utilisateur
+- créer/supprimer/modifier un sujet de discussion
+- créer/supprimer/modifier un message de n'importe quel utilisateur
+- suspendre un compte utilisateur
+
+## Un admin peut :
+- faire toutes les actions possibles par un modérateur
+- créer/supprimer/modifier une catégorie de discussion
+- supprimer un compte utilisateur
+- suspendre un compte utilisateur
+
---
+++ prisma/migrations/20230517165351_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "urlAvatar" TEXT,
+    "role" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Topic" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "categoryId" INTEGER NOT NULL,
+
+    CONSTRAINT "Topic_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "published" BOOLEAN NOT NULL DEFAULT true,
+    "authorId" INTEGER NOT NULL,
+    "topicId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_authorId_key" ON "Post"("authorId");
+
+-- AddForeignKey
+ALTER TABLE "Topic" ADD CONSTRAINT "Topic_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_topicId_fkey" FOREIGN KEY ("topicId") REFERENCES "Topic"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
