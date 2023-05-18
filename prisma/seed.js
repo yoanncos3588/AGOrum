354f2f6544d7e0bf35b764da9a0f9adcc6c47c7f
YoannCos <yoanncos@gmail.com>
2023-05-18 15:26:25 +0200

    ajout de fausses données dans seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,62 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient()
+
 async function main() {
-    const alice = await prisma.user.upsert({
-        where: { email: 'alice@prisma.io' },
-        update: {},
-        create: {
-            email: 'alice@prisma.io',
-            name: 'Alice',
-            password: 'bbb',
-            urlAvatar: 'www',
-            role: 'member',
-            post: undefined
+    const fakeData = await prisma.category.create({
+        data:
+        {
+            name: `Sports`,
+            description: `In laoreet nibh non quam vehicula, et tincidunt leo varius.`,
+            topic: {
+                create: [
+                    {
+                        name: `Pourquoi les chats retombent toujours sur leurs pattes ?`,
+                        post: {
+                            create: [
+                                {
+                                    content: `Je ne sais pas, drole de question :)`,
+                                    author: {
+                                        create: {
+                                            email: `michel@aol.com`,
+                                            name: `Michel Zabrouk`,
+                                            password: `azerety`,
+                                            urlAvatar: `www`,
+                                            role: `member`,
+                                        },
+                                    },
+                                },
+                                {
+                                    content: `Ils sont cons sans doute`,
+                                    author: {
+                                        create: {
+                                            email: `justine@yahoo.com`,
+                                            name: `Justine56`,
+                                            password: `azerety`,
+                                            urlAvatar: `www`,
+                                            role: `member`,
+                                        },
+                                    },
+                                },
+                                {
+                                    content: `Vraiment tu es désagréable`,
+                                    author: {
+                                        create: {
+                                            email: `pascal@yahoo.com`,
+                                            name: `Pascal23`,
+                                            password: `azerety`,
+                                            urlAvatar: `www`,
+                                            role: `member`,
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                ],
+            },
         },
     })
-    console.log({ alice })
 }
 main()
     .then(async () => {
